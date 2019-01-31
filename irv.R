048cc50f01fc64fa37f1e3ee1e12f9a8be29cbd4
Jarad Niemi <niemi@iastate.edu>
2019-01-31 11:00:03 -0600

    Update script for current use

--- irv.R
+++ irv.R
@@ -1,10 +1,28 @@
 
-
 rerank <- function(d) {
   for (v in 1:nrow(d)) {
     d[v,] <- as.numeric(as.factor(as.numeric(d[v,])))
   }
   return(d)
+}
+
+# From https://stackoverflow.com/questions/17288197/reading-a-csv-file-organized-horizontally
+read.tcsv = function(file, header=TRUE, sep=",", ...) {
+  
+  n = max(count.fields(file, sep=sep), na.rm=TRUE)
+  x = readLines(file)
+  
+  .splitvar = function(x, sep, n) {
+    var = unlist(strsplit(x, split=sep))
+    length(var) = n
+    return(var)
+  }
+  
+  x = do.call(cbind, lapply(x, .splitvar, sep=sep, n=n))
+  x = apply(x, 1, paste, collapse=sep) 
+  out = read.csv(text=x, sep=sep, header=header, ...)
+  return(out)
+  
 }
 
 

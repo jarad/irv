fe960b7dd47f3616c26468b43a539c58d4adbb06
Jarad Niemi <niemi@iastate.edu>
2023-09-19 13:41:02 -0500

    Use n_choices in fake_data

--- fake_data.R
+++ fake_data.R
@@ -1,7 +1,9 @@
 # Create fake voter data
 n_voters <- 35
-ranks <- matrix(NA, ncol=5, nrow=n_voters)
-for (i in 1:n_voters) ranks[i,] <- sample(5)
+n_choices <- 4
+
+ranks <- matrix(NA, ncol = n_choices, nrow = n_voters)
+for (i in 1:n_voters) ranks[i,] <- sample(n_choices, prob = n_choices:1)
 
 ranks <- as.data.frame(ranks)
-colnames(ranks) <- paste("candidate", LETTERS[1:5], sep="")
+colnames(ranks) <- paste("option", LETTERS[1:n_choices], sep="")
